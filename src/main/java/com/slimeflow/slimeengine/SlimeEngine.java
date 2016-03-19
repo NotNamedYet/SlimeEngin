7bb0d08471adcaeabe1912177a4d807d7d9dd77c
BuildTools <x9litch@gmail.com>
2016-03-19 04:42:02 +0100

    Project Update

---
+++ src/main/java/com/slimeflow/slimeengine/SlimeEngine.java
@@ -0,0 +1,21 @@
+package com.slimeflow.slimeengine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by x9litch on 19/03/2016. - slimeflow.com
+ */
+public class SlimeEngine extends JavaPlugin
+{
+    @Override
+    public void onDisable()
+    {
+
+    }
+
+    @Override
+    public void onEnable()
+    {
+
+    }
+}
