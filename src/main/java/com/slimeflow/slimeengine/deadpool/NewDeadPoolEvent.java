d4835b7c91ed0fc816014ea774e90e3c4023de62
BuildTools <x9litch@gmail.com>
2016-03-19 06:53:16 +0100

    Update SlimeEngine & DeadPool

---
+++ src/main/java/com/slimeflow/slimeengine/deadpool/NewDeadPoolEvent.java
@@ -0,0 +1,39 @@
+package com.slimeflow.slimeengine.deadpool;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import java.util.UUID;
+
+/**
+ * Created by x9litch on 19/03/2016. - slimeflow.com
+ */
+public class NewDeadPoolEvent extends Event {
+
+    private final DeadPoolData m_data;
+
+    public NewDeadPoolEvent(DeadPoolData data)
+    {
+        m_data = data;
+    }
+
+    public UUID getUniqueId()
+    {
+        return m_data.m_id;
+    }
+
+    public double getAmount()
+    {
+        return m_data.m_poolAmount;
+    }
+
+    public void setAmount(double amount)
+    {
+        m_data.m_poolAmount = amount;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return null;
+    }
+}
